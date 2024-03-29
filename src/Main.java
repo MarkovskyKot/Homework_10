965036fe3dc0adec625337c3deb6e042e1202c75
Markovsky_Kot <0postaldude0@gmail.com>
2024-03-29 22:02:08 +0700

    Homework completed

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         //Задача 1
-        String firstName="Ivan";
-        String middleName="Ivanovich";
-        String lastName="Ivanov";
-        String fullName=lastName+" "+firstName+" "+middleName;
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника - " + fullName);
         System.out.println();
         //Задача 2
-        System.out.println("Данные ФИО сотрудника для заполнения отчета - "+fullName.toUpperCase());
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());
         System.out.println();
         //Задача 3
-        String fullName1="Иванов Семён Семёнович";
-        String replacedFullName=fullName1.replace('ё','е');
-        System.out.println("Данные ФИО сотрудника - "+replacedFullName);
+        String fullName1 = "Иванов Семён Семёнович";
+        String replacedFullName = fullName1.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника - " + replacedFullName);
     }
 }
